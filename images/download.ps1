074f04a6d5571b0d6b3f8f873034869ab4284983
schneider-wangyu <schneider-wangyu@users.noreply.github.com>
2026-07-28 15:44:31 +0800

    Site update 2026-07-28T07:44:31.721Z

--- images/download.ps1
+++
@@ -1,4 +0,0 @@
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-$img = Invoke-WebRequest -Uri 'https://images.unsplash.com/photo-1558494949-ef010cbdcc31?w=800' -UseBasicParsing
-[System.IO.File]::WriteAllBytes('C:\Users\Administrator\Documents\schneider-blog\source\images\edge-tech-202606021051.jpg', $img.Content)
-Write-Host "Download completed"
